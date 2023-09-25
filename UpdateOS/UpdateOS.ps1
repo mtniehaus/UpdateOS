45269d175a081165e740c5931a2bc0ce978987e8
Michael Niehaus <niehaus@live.com>
2023-09-25 07:48:47 -0700

    Updated for Windows 11

--- UpdateOS/UpdateOS.ps1
+++ UpdateOS/UpdateOS.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.4
+.VERSION 1.5
 
 .GUID 07e4ef9f-8341-4dc4-bc73-fc277eb6b4e6
 
@@ -25,6 +25,7 @@
 .EXTERNALSCRIPTDEPENDENCIES 
 
 .RELEASENOTES
+Version 1.5:  Improved logging, reboot logic.
 Version 1.4:  Fixed reboot logic.
 Version 1.3:  Force use of Microsoft Update/WU.
 Version 1.2:  Updated to work on ARM64.
@@ -35,16 +36,17 @@ Version 1.0:  Original published version.
 
 <#
 .SYNOPSIS
-Installs the latest Windows 10 quality updates.
+Installs the latest Windows 10/11 quality updates.
 .DESCRIPTION
-This script uses the PSWindowsUpdate module to install the latest cumulative update for Windows 10.
+This script uses the PSWindowsUpdate module to install the latest cumulative update for Windows 10/11.
 .EXAMPLE
 .\UpdateOS.ps1
 #>
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$False)] [Switch] $HardReboot = $false
+    [Parameter(Mandatory=$False)] [ValidateSet('Soft', 'Hard', 'None', 'Delayed')] [String] $Reboot = 'Delayed',
+    [Parameter(Mandatory=$False)] [Int32] $RebootTimeout = 120
 )
 
 Process
@@ -55,7 +57,7 @@ if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
 {
     if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
     {
-        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath"
+        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath" -Reboot $Reboot -RebootTimeout $RebootTimeout
         Exit $lastexitcode
     }
 }
@@ -72,41 +74,47 @@ Start-Transcript "$($env:ProgramData)\Microsoft\UpdateOS\UpdateOS.log"
 
 # Main logic
 $needReboot = $false
-Write-Host "Installing updates with HardReboot = $HardReboot."
 
 # Load module from PowerShell Gallery
+$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+Write-Host "$ts Importing NuGet and PSWindowsUpdate"
 $null = Install-PackageProvider -Name NuGet -Force
 $null = Install-Module PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 
 # Install all available updates
-Get-WindowsUpdate -Install -IgnoreUserInput -AcceptAll -WindowsUpdate -IgnoreReboot | Select Title, KB, Result | Format-Table
+$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+Write-Host "$ts Installing updates."
+Install-WindowsUpdate -AcceptAll -IgnoreReboot -Verbose | Select Title, KB, Result | Format-Table
 $needReboot = (Get-WURebootStatus -Silent).RebootRequired
 
 # Specify return code
-if ($needReboot)
-{
-    Write-Host "Windows Update indicated that a reboot is needed."
-}
-else
-{
-    Write-Host "Windows Update indicated that no reboot is required."
+$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+if ($needReboot) {
+    Write-Host "$ts Windows Update indicated that a reboot is needed."
+} else {
+    Write-Host "$ts Windows Update indicated that no reboot is required."
 }
 
 # For whatever reason, the reboot needed flag is not always being properly set.  So we always want to force a reboot.
 # If this script (as an app) is being used as a dependent app, then a hard reboot is needed to get the "main" app to
 # install.
-if ($HardReboot)
-{
-    Write-Host "Exiting with return code 1641 to indicate a hard reboot is needed."
+$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+if ($Reboot -eq "Hard") {
+    Write-Host "$ts Exiting with return code 1641 to indicate a hard reboot is needed."
     Stop-Transcript
     Exit 1641
-}
-else
-{
-    Write-Host "Exiting with return code 3010 to indicate a soft reboot is needed."
+} elseif ($Reboot -eq "Soft") {
+    Write-Host "$ts Exiting with return code 3010 to indicate a soft reboot is needed."
     Stop-Transcript
     Exit 3010
+} elseif ($Reboot -eq "Delayed") {
+    Write-Host "$ts Rebooting with a $RebootTimeout second delay"
+    & shutdown.exe /r /t $RebootTimeout /c "Rebooting to complete the installation of Windows updates."
+    Exit 0
+} else {
+    Write-Host "$ts Skipping reboot based on Reboot parameter (None)"
+    Exit 0
 }
 
 }
