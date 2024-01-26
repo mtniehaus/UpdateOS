ab9eca5f9fcd642b1e5123f488c161fc72b76ce3
Michael Niehaus <niehaus@live.com>
2024-01-26 01:08:39 -0800

    Switch to using COM objects directly instead of PSWindowsUpdate, add driver logic.

--- UpdateOS/UpdateOS.ps1
+++ UpdateOS/UpdateOS.ps1
@@ -46,79 +46,117 @@ This script uses the PSWindowsUpdate module to install the latest cumulative upd
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$False)] [ValidateSet('Soft', 'Hard', 'None', 'Delayed')] [String] $Reboot = 'Soft',
-    [Parameter(Mandatory=$False)] [Int32] $RebootTimeout = 120
+    [Parameter(Mandatory = $False)] [ValidateSet('Soft', 'Hard', 'None', 'Delayed')] [String] $Reboot = 'Soft',
+    [Parameter(Mandatory = $False)] [Int32] $RebootTimeout = 120,
+    [Parameter(Mandatory = $False)] [switch] $ExcludeDrivers
 )
 
-Process
-{
+Process {
 
-# If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
-if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
-{
-    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
-    {
-        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath" -Reboot $Reboot -RebootTimeout $RebootTimeout
-        Exit $lastexitcode
+    # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
+    if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64") {
+        if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe") {
+            & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath" -Reboot $Reboot -RebootTimeout $RebootTimeout
+            Exit $lastexitcode
+        }
     }
-}
 
-# Create a tag file just so Intune knows this was installed
-if (-not (Test-Path "$($env:ProgramData)\Microsoft\UpdateOS"))
-{
-    Mkdir "$($env:ProgramData)\Microsoft\UpdateOS"
-}
-Set-Content -Path "$($env:ProgramData)\Microsoft\UpdateOS\UpdateOS.ps1.tag" -Value "Installed"
+    # Create a tag file just so Intune knows this was installed
+    if (-not (Test-Path "$($env:ProgramData)\Microsoft\UpdateOS")) {
+        Mkdir "$($env:ProgramData)\Microsoft\UpdateOS"
+    }
+    Set-Content -Path "$($env:ProgramData)\Microsoft\UpdateOS\UpdateOS.ps1.tag" -Value "Installed"
 
-# Start logging
-Start-Transcript "$($env:ProgramData)\Microsoft\UpdateOS\UpdateOS.log"
+    # Start logging
+    Start-Transcript "$($env:ProgramData)\Microsoft\UpdateOS\UpdateOS.log"
 
-# Main logic
-$needReboot = $false
+    # Main logic
+    $script:needReboot = $false
 
-# Load module from PowerShell Gallery
-$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-Write-Host "$ts Importing NuGet and PSWindowsUpdate"
-$null = Install-PackageProvider -Name NuGet -Force
-$null = Install-Module PSWindowsUpdate -Force
-Import-Module PSWindowsUpdate
+    # Opt into Microsoft Update
+    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+    Write-Output "$ts Opting into Microsoft Update"
+    $ServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
+    $ServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"
+    $ServiceManager.AddService2($ServiceId, 7, "") | Out-Null
 
-# Opt into Microsoft Update
-Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -AddServiceFlag 7 -Confirm:$False
+    # Install all available updates
+    $WUDownloader = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
+    $WUInstaller = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
+    if ($ExcludeDrivers) {
+        $queries = @("IsInstalled=0 and Type='Software'")
+    }
+    else {
+        $queries = @("IsInstalled=0 and Type='Software'", "IsInstalled=0 and Type='Driver'")
+    }
 
-# Install all available updates
-$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-Write-Host "$ts Installing updates."
-Install-WindowsUpdate -AcceptAll -IgnoreReboot -Verbose | Select Title, KB, Result | Format-Table
-$needReboot = (Get-WURebootStatus -Silent).RebootRequired
+    $queries | ForEach-Object {
 
-# Specify return code
-$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-if ($needReboot) {
-    Write-Host "$ts Windows Update indicated that a reboot is needed."
-} else {
-    Write-Host "$ts Windows Update indicated that no reboot is required."
-}
+        $WUUpdates = New-Object -ComObject Microsoft.Update.UpdateColl
+        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Write-Host "$ts Getting $_ updates."        
+        ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search($_)).Updates | ForEach-Object {
+            if (!$_.EulaAccepted) { $_.EulaAccepted = $true }
+            if ($_.Title -notmatch "Preview") { [void]$WUUpdates.Add($_) }
+        }
 
-# For whatever reason, the reboot needed flag is not always being properly set.  So we always want to force a reboot.
-# If this script (as an app) is being used as a dependent app, then a hard reboot is needed to get the "main" app to
-# install.
-$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-if ($Reboot -eq "Hard") {
-    Write-Host "$ts Exiting with return code 1641 to indicate a hard reboot is needed."
-    Stop-Transcript
-    Exit 1641
-} elseif ($Reboot -eq "Soft") {
-    Write-Host "$ts Exiting with return code 3010 to indicate a soft reboot is needed."
-    Stop-Transcript
-    Exit 3010
-} elseif ($Reboot -eq "Delayed") {
-    Write-Host "$ts Rebooting with a $RebootTimeout second delay"
-    & shutdown.exe /r /t $RebootTimeout /c "Rebooting to complete the installation of Windows updates."
-    Exit 0
-} else {
-    Write-Host "$ts Skipping reboot based on Reboot parameter (None)"
-    Exit 0
-}
+        if ($WUUpdates.Count -ge 1) {
+            $WUInstaller.ForceQuiet = $true
+            $WUInstaller.Updates = $WUUpdates
+            $WUDownloader.Updates = $WUUpdates
+            $UpdateCount = $WUDownloader.Updates.count
+            if ($UpdateCount -ge 1) {
+                $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+                Write-Output "$ts Downloading $UpdateCount Updates"
+                foreach ($update in $WUInstaller.Updates) { Write-Output "$($update.Title)" }
+                $Download = $WUDownloader.Download()
+            }
+            $InstallUpdateCount = $WUInstaller.Updates.count
+            if ($InstallUpdateCount -ge 1) {
+                $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+                Write-Output "$ts Installing $InstallUpdateCount Updates"
+                $Install = $WUInstaller.Install()
+                $ResultMeaning = ($Results | Where-Object { $_.ResultCode -eq $Install.ResultCode }).Meaning
+                Write-Output $ResultMeaning
+                $script:needReboot = $Install.RebootRequired
+            } 
+        }
+        else {
+            Write-Output "No Updates Found"
+        } 
+    }
+
+    # Specify return code
+    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+    if ($script:needReboot) {
+        Write-Host "$ts Windows Update indicated that a reboot is needed."
+    }
+    else {
+        Write-Host "$ts Windows Update indicated that no reboot is required."
+    }
+
+    # For whatever reason, the reboot needed flag is not always being properly set.  So we always want to force a reboot.
+    # If this script (as an app) is being used as a dependent app, then a hard reboot is needed to get the "main" app to
+    # install.
+    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+    if ($Reboot -eq "Hard") {
+        Write-Host "$ts Exiting with return code 1641 to indicate a hard reboot is needed."
+        Stop-Transcript
+        Exit 1641
+    }
+    elseif ($Reboot -eq "Soft") {
+        Write-Host "$ts Exiting with return code 3010 to indicate a soft reboot is needed."
+        Stop-Transcript
+        Exit 3010
+    }
+    elseif ($Reboot -eq "Delayed") {
+        Write-Host "$ts Rebooting with a $RebootTimeout second delay"
+        & shutdown.exe /r /t $RebootTimeout /c "Rebooting to complete the installation of Windows updates."
+        Exit 0
+    }
+    else {
+        Write-Host "$ts Skipping reboot based on Reboot parameter (None)"
+        Exit 0
+    }
 
 }
